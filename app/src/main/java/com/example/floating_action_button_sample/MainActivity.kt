43d9c9800030bae387042661376bf5154909ddc0
Veget-ables <makuhari109@gmail.com>
2022-04-06 11:34:17 +0900

    initial commit

---
+++ app/src/main/java/com/example/floating_action_button_sample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.floating_action_button_sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.floating_action_button_sample.ui.theme.FloatingactionbuttonsampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FloatingactionbuttonsampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FloatingactionbuttonsampleTheme {
+        Greeting("Android")
+    }
+}
